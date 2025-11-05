1fa93abf89e392538af63fb42ff599fd0c6fcf41
KenjiMobutu <kenjimobutu@gmail.com>
2025-11-05 17:55:44 +0100

    CORRECTION19

--- src/app/api/checkout_sessions/route.ts
+++ src/app/api/checkout_sessions/route.ts
@@ -1,8 +1,9 @@
+
 import Stripe from 'stripe';
 import { NextRequest, NextResponse } from 'next/server';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2023-08-16',
+  apiVersion: '2025-06-30.basil',
 });
 
 export async function POST(req: NextRequest) {
